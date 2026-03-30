d6deb51b1f8903036a8efc9b4b8ce735603147f8
MatteoCastigliego <s314196@studenti.polito.it>
2026-03-30 17:11:13 +0200

    feat: ex01 completed

---
+++ lab02/es01/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    int id, mark;
+    long code;
+    char surname[31], name[31];
+    ifstream filein;
+    ofstream fileout3;
+    fstream fileout2;
+
+    filein.open("file_1.txt", ios::in);
+    if(!filein.is_open()){
+        cout<<"Errore nell'apertura del file di input";
+        return EXIT_FAILURE;
+    }
+
+    fileout2.open("file_2.txt", ios::out | ios::binary | ios::in);
+    if(!fileout2.is_open()){
+        cout<<"Errore nell'apertura del file di output 2";
+        return EXIT_FAILURE;
+    }
+
+    fileout3.open("file_3.txt", ios::out);
+    if(!fileout3.is_open()){
+        cout<<"Errore nell'apertura del file di output 3";
+        return EXIT_FAILURE;
+    }
+
+    while(filein >> id >> code >> surname >> name >> mark){
+        fileout2.write(reinterpret_cast<const char*>(&id), sizeof(id));
+        fileout2.write(reinterpret_cast<const char*>(&code), sizeof(code));
+        fileout2.write(reinterpret_cast<const char*>(&surname), sizeof(surname));
+        fileout2.write(reinterpret_cast<const char*>(&name), sizeof(name));
+        fileout2.write(reinterpret_cast<const char*>(&mark), sizeof(mark));
+    }
+    filein.close();
+    fileout2.close();
+
+    fileout2.open("file_2.txt", ios::out | ios::binary | ios::in);
+    while(fileout2.read(reinterpret_cast<char*>(&id), sizeof(id))){
+        fileout2.read(reinterpret_cast<char*>(&code), sizeof(code));
+        fileout2.read(reinterpret_cast<char*>(&surname), sizeof(surname));
+        fileout2.read(reinterpret_cast<char*>(&name), sizeof(name));
+        fileout2.read(reinterpret_cast<char*>(&mark), sizeof(mark));
+
+        if(!fileout2.eof()){
+            fileout3 << id << " " << code << " " << surname << " " << name << " " << mark << endl;
+        }
+    }
+}
